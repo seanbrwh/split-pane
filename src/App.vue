cb5f1e9dd95333c8ef071b4a20cbce2ea05aa652
Sean White <seanbrwh@gmail.com>
2019-05-22 17:32:25 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">        
+    <button @click="splitDir">Change Direction</button>
+    <split-Pane :split="splitDirection">
+      <div slot="pane1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur maxime, eum cumque dignissimos, perspiciatis veniam, quae expedita provident veritatis repudiandae sunt. Modi expedita cum, sint tempore aspernatur nulla recusandae voluptate!
+      Reprehenderit aliquam quam voluptate optio eligendi atque maxime, sint mollitia voluptas magni, sit veniam sequi alias ab est nostrum quae eveniet tempore quos, necessitatibus repudiandae unde dolorum. Numquam, eveniet est!
+      Soluta ipsum vel perspiciatis ipsa incidunt, totam expedita magnam ex nihil quibusdam modi vitae animi, dolore eaque possimus consequatur iste. Explicabo culpa est saepe aliquid modi, molestias ipsa id assumenda?
+      Eum, culpa quidem quas magnam quos vero aut, delectus non qui sint aspernatur ipsa cumque dolores saepe sapiente reprehenderit illo optio voluptas laudantium doloribus id libero at? Officia, reprehenderit quis!
+      Autem vitae itaque modi exercitationem laborum corrupti reprehenderit quod beatae est, magni ullam. Deserunt architecto rerum tenetur omnis! Odio sapiente labore necessitatibus dolorem porro molestiae optio recusandae impedit harum autem!
+      Vero in optio officiis id repellat voluptate atque, modi sequi hic iste voluptas ad, unde, nihil incidunt aliquid. Ipsum soluta quisquam eius quidem tenetur explicabo exercitationem ullam maxime nesciunt magni!
+      Tempore accusantium omnis facere soluta cumque suscipit dolore eius, cum hic laboriosam maxime, vitae placeat quis quam ipsa assumenda fugiat quae provident, inventore blanditiis excepturi consequatur exercitationem. Eaque, quasi accusantium.
+      Ullam fugit exercitationem, dolore non asperiores, architecto in maxime labore fugiat sed quo consectetur numquam odit tenetur repellat dolorem ab maiores! Ipsa ipsum perferendis, blanditiis sint quidem laudantium molestias asperiores.
+      Explicabo quae, optio ut enim adipisci culpa dolor, rem odit inventore fugiat aut atque harum assumenda nostrum sequi non iusto fugit quia illo quod magni provident eaque quo reprehenderit? Ad.
+      Quas dolorum a, qui reiciendis quam alias ut id autem unde voluptatem deserunt omnis dignissimos itaque, ullam dolore sed dicta animi minima voluptatibus doloribus. Blanditiis maiores illo id quibusdam aliquid?
+      Distinctio ea eveniet sit delectus rerum ex magni laborum necessitatibus, sequi modi ut, odio numquam quas id impedit quo voluptates perspiciatis incidunt dolore nulla, perferendis porro. Vel expedita ut placeat.
+      Nostrum modi neque saepe illum reiciendis laboriosam distinctio tenetur, voluptas est quis eaque animi perspiciatis asperiores aliquam corrupti voluptatem fugiat. Totam id quidem explicabo, rerum labore molestiae culpa itaque rem.
+      Suscipit saepe facere sapiente vitae accusantium corrupti ipsum delectus repellat commodi culpa magni sunt, accusamus omnis debitis non qui, quo voluptatem officiis dolorum blanditiis quos? Quaerat amet ducimus rerum inventore.
+      Blanditiis ad, deserunt ullam nobis tenetur at doloremque asperiores iure repudiandae recusandae voluptatum excepturi assumenda nemo nam reiciendis ipsam, similique ut illum cupiditate voluptates quam voluptatibus incidunt aliquam. Voluptas, quia.
+      Voluptatum obcaecati in ipsum! Quae eius explicabo dolores provident ab totam ipsum molestiae eaque nisi saepe, quisquam vero nemo animi veniam vitae quidem laborum quas laboriosam ipsa est ea ad?
+      Rem, beatae necessitatibus quo architecto reprehenderit eius corporis tempore nobis, laudantium ex sapiente labore dignissimos. Quos inventore, optio deleniti repellendus voluptas officiis suscipit amet, vel quasi nemo, quas quisquam cum.
+      Consectetur vel ipsa consequuntur ducimus reprehenderit, quod deserunt placeat earum, a repellendus quas eaque! Distinctio dolorem voluptas quidem laudantium soluta sequi vero libero, nemo, totam maxime odit asperiores, alias ipsam?
+      Amet rerum numquam, voluptate veniam ratione quam. Quaerat, recusandae doloribus pariatur dolores beatae exercitationem libero, officia modi deleniti neque asperiores illum totam, dolorum odio. Tempora aut dicta nemo voluptate ipsa.
+      Doloribus temporibus eaque, expedita ipsa recusandae quod, similique ex itaque perferendis unde quae culpa laboriosam sint a repellendus illo. Quisquam vero perferendis, facere dolorum consequuntur facilis error doloribus provident omnis.
+      Repellendus odio quisquam expedita aliquam sed iure voluptatibus architecto adipisci, ducimus recusandae nemo nisi deserunt consectetur tenetur est explicabo facere hic perferendis molestiae sit eius modi labore esse aspernatur! Minima.</div>
+      <div slot="pane2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum blanditiis minima veritatis quisquam neque necessitatibus vel similique porro? Explicabo odit veniam veritatis assumenda earum, unde quis. Laborum sequi neque modi.
+      Voluptatum pariatur ducimus nesciunt, repellendus ad quaerat harum nostrum laboriosam ab beatae asperiores? Natus magnam sequi deserunt harum non delectus quis consectetur nesciunt eveniet! Blanditiis, illo. Ducimus recusandae odit aliquam!
+      Ut nostrum voluptates necessitatibus debitis, esse aut tempora quia eius reiciendis voluptatibus odio. Amet mollitia ea labore corrupti dolor quidem accusamus sunt, laborum quia esse eius culpa iusto architecto sapiente?
+      Iste ipsa molestiae sunt asperiores atque, magnam adipisci pariatur quaerat, accusantium natus omnis, odio eaque! Laudantium earum adipisci, tenetur in excepturi voluptas, praesentium quidem, dolorem dolores quibusdam aliquam amet natus!
+      Delectus natus assumenda deleniti repudiandae doloremque eos perferendis voluptas error minima voluptate obcaecati ipsa laudantium illum ad rerum, numquam ratione, harum deserunt sapiente quasi tempora consequuntur eligendi blanditiis. Quod, totam?
+      Vero beatae illum expedita et ipsum veniam at ratione aliquam odit ullam impedit corporis consequatur excepturi tenetur modi doloremque repellendus minus, incidunt quia magni blanditiis est soluta? Nam, ratione unde!
+      Nemo quae reiciendis illum nostrum non est dolores velit dolore, quis consequuntur ipsam fugit hic culpa soluta ut id eveniet explicabo alias, sint atque voluptates laborum facilis earum? Dolores, sequi?
+      Iste eius minima quod est veniam, blanditiis iure earum nihil, voluptates dicta vero quam recusandae, eaque sunt? Ut laborum, minima nisi incidunt corrupti blanditiis rerum ratione labore iste officia. Tempore?
+      Tempore, voluptate! Ea ex, est obcaecati sint architecto aperiam sequi veritatis harum, excepturi, vel sapiente blanditiis error in maiores voluptatibus praesentium fuga voluptatum odit iste incidunt asperiores sunt ab exercitationem.
+      Laboriosam, perferendis fuga eveniet illo et voluptates. Modi, quas facere vitae reiciendis fugit aliquam assumenda. Omnis nihil sapiente nam deleniti asperiores. Optio pariatur placeat vel eaque et ipsam, voluptatem nostrum!
+      Ipsa beatae reprehenderit, aliquid, nobis perspiciatis nemo inventore reiciendis est distinctio eveniet veniam repellat aspernatur quas officia, accusamus ex dignissimos quasi alias recusandae libero ducimus a? Sint optio quaerat deleniti.
+      Facere possimus eum enim animi explicabo eos culpa ipsa, nemo quam soluta deleniti molestias aperiam a cumque voluptatem reprehenderit saepe praesentium voluptatibus non hic! Temporibus reprehenderit corrupti rerum sint ratione.
+      Iste cupiditate consequuntur ipsa illo consequatur aliquid earum facilis nemo libero, porro temporibus perferendis aliquam quia quam incidunt, iusto eaque provident vel repellendus itaque quo? Omnis fuga minima nemo eveniet!
+      Perspiciatis laboriosam nisi fuga cupiditate asperiores dolorem placeat, nihil sapiente vero voluptatem fugit, soluta qui? Pariatur, officia molestiae. Placeat molestiae laboriosam mollitia officiis laudantium. Dolores ex magnam dolor iure totam!
+      Illum minima qui eaque tenetur laudantium quis nemo voluptatum minus dicta incidunt. Quasi nobis, architecto, suscipit, odio voluptate eum eveniet explicabo animi rerum excepturi dolor error vero maxime ullam ratione?
+      Quo aliquid minima distinctio, nulla quibusdam quod, omnis non voluptate rerum perferendis necessitatibus voluptas incidunt doloribus sapiente! Nesciunt error suscipit eligendi, magni odio autem quibusdam ea, eveniet culpa cum soluta?
+      Eveniet minima, voluptatem facilis sapiente repellat asperiores officia sit alias quam id quod architecto tempora numquam, veritatis sequi assumenda vel, maiores similique fugit corporis blanditiis? Beatae iusto facere eum aliquid!
+      Nemo omnis dolore ipsum ab ex, molestiae cupiditate delectus voluptatum dolor exercitationem. Dignissimos quos omnis iure provident quidem vero aperiam placeat eum id dolor, magnam possimus praesentium consequuntur. Excepturi, ipsam.
+      Sequi expedita molestiae laboriosam harum reiciendis tempora eius quis. Dolore nesciunt esse beatae ipsum a, id ducimus alias enim quisquam ullam, aut vero similique maxime quas fuga placeat, repudiandae fugit!
+      Similique cupiditate placeat libero, quisquam, pariatur beatae rem soluta dolorem reiciendis error labore repudiandae, eveniet illum porro. Debitis dolore quibusdam, quaerat ab, ipsa quasi a sed error delectus minima molestiae.</div>
+      </split-Pane>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+import splitPane from './components/splitPane'
+export default {       
+  components:{
+    splitPane:splitPane
+  },
+  data(){
+    return{
+      splitDirection:'vertical'
+    }
+  },
   name: 'app',
-  components: {
-    HelloWorld
+  methods:{
+    splitDir(){
+      return this.splitDirection === 'vertical' ? this.splitDirection = 'horizontal' : this.splitDirection = 'vertical'      
+    }
   }
 }
 </script>
---
+++ src/components/splitPane.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :class="[split == 'vertical' ? 'vertical' : '',split=='horizontal'? 'horizontal': '']" >
+        <pane  :class="[split == 'vertical' ? 'vertical-pane' : '',split=='horizontal'? 'horizontal-pane': '']">
+            <slot name='pane1'></slot>
+        </pane>
+        <pane  :class="[split == 'vertical' ? 'vertical-pane' : '',split=='horizontal'? 'horizontal-pane': '']">
+            <slot name='pane2'></slot>
+        </pane>
+    </div>
+</template>
+
+<script>
+import pane from './pane'
+export default {
+    props:{
+        split:String
+    },
+    components:{
+        pane
+    }
+}
+</script>
+
+<style>
+    .splitPane{
+        width:100%;
+        height: 100%;
+    }   
+    .vertical{
+        display: flex;
+        justify-content: space-evenly;
+        flex-direction: row;
+    }
+    .vertical-pane{
+        width:50%;
+        height:80vh;
+    }
+    .horizontal-pane{
+        height:40vh;
+    }
+    .horinzontal{
+        display: flex;
+        justify-content: space-evenly;
+        flex-direction: column;
+    }
+</style>
